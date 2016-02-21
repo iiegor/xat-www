a6c19a7e22dd34abcf2e70d9b8e0a61d8b29b5c7
Iegor Azuaga <dextrackmedia@gmail.com>
2016-02-21 18:44:24 +0100

    Add static folder and terms.php

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('xat', ['ngMaterial']);
+
+app.controller('HomeController', function() {
+  console.log('Home')
+});
+
+app.controller('LoginController', function() {
+  console.log('Login')
+});
